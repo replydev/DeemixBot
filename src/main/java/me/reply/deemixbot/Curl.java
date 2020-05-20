e63ef7b7db89d82f72b4f6bd75bf2b0031244707
replydev <replydev@protonmail.com>
2020-05-20 23:49:01 +0200

    Curl class to use deezer api

---
+++ src/main/java/me/reply/deemixbot/Curl.java
@@ -0,0 +1,35 @@
+package me.reply.deemixbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Curl {
+
+    private final String link;
+    private static final Logger logger = LoggerFactory.getLogger(Curl.class);
+
+    public Curl(String url){
+        this.link = url;
+    }
+
+    public String run() throws MalformedURLException {
+        URL url = new URL(this.link);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                builder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
